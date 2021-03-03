212f72251d47bf8037e944d8035e6f090542f0d3
Richard Grant <admin@richard-grant.com>
2021-03-02 19:40:14 -0800

    feat: 🎸 base template
    integrated angular flex layout, installed hammerjs

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ViewChild } from '@angular/core';
+import { MatDrawerContainer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  isExpanded = true;
+  @ViewChild("drawerContainer") container: MatDrawerContainer;
+  constructor(){}
+  log(a: any){
+    console.log(a);
+  }
+
+  update(){
+    
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,8 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexModule } from '@angular/flex-layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list';
 
 const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatListModule
 ];
 
 
@@ -15,6 +27,7 @@ const material = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FlexModule,
     ...material
   ],
   providers: [],
